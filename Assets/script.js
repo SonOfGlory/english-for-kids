91407920059cb30ead4d31208859e7aaf815df9b
Denys Marytchak <denys.marytchak@gmail.com>
2020-04-16 21:42:17 +0200

    feat: add audio files

---
+++ Assets/script.js
@@ -0,0 +1,16 @@
+/* === Toggler low-resolution === */
+
+const menu_toggler = document.querySelector("#header > div.menu-toggler");
+const menu_togglers_set = document.querySelectorAll('[data-menuswitch]');
+
+menu_toggler.onclick = () => {
+  if (document.querySelectorAll('[data-menuswitch="hidden"]').length === 0) {
+    menu_togglers_set.forEach(toggler => {
+      toggler.dataset.menuswitch = 'hidden';
+    }
+  )}
+  else menu_togglers_set.forEach(toggler => {
+      toggler.dataset.menuswitch = '';
+    }
+  )
+}
